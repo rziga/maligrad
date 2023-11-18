8dd363bab79a99195a3a24f54c8ff89457dfb3a7
Ziga R <ziga.mlearning@gmail.com>
2023-11-18 19:10:51 +0100

    moar ops

--- autograd/engine.py
+++ autograd/engine.py
@@ -46,7 +46,7 @@ class DataNode(Node):
     def backward(self, partial: ndarray | None = None) -> None:
         # start backprop
         if partial is None:
-            assert self.data.squeeze().ndim == 0, "bacprop without partial grad can be only started from scalars"
+            assert self.data.squeeze().ndim == 0, "backprop without partial gradient can be only started from scalars"
             partial = 1.
         self.grad += partial
         graph_nodes = [n for n in reversed(self.toposort()) if isinstance(n, FunctionNode)]
@@ -65,7 +65,7 @@ class DataNode(Node):
             self.grad += grad.sum(axis=to_sum).squeeze()
     
     @staticmethod
-    def promote(data: "DataNode" | Any):
+    def promote(data: "DataNode" | Any) -> "DataNode":
         if isinstance(data, DataNode):
             return data
         return DataNode(data)
@@ -81,28 +81,44 @@ class DataNode(Node):
     # OP OVERRIDES
 
     def __add__(self, other: "DataNode" | Any) -> "DataNode":
-        other = self.promote(other)
         add_node = Add()
         return add_node(self, other)
     
+    def __radd__(self, other: "DataNode" | Any) -> "DataNode":
+        return self + other
+    
     def __mul__(self, other: "DataNode" | Any) -> "DataNode":
-        other = self.promote(other)
         mul_node = Mul()
         return mul_node(self, other)
+    
+    def __rmul__(self, other: "DataNode" | Any) -> "DataNode":
+        return self * other
     
     def __matmul__(self, other: "DataNode" | Any) -> "DataNode":
         other = self.promote(other)
         matmul_node = Matmul()
         return matmul_node(self, other)
     
+    def __rmatmul__(self, other: "DataNode" | Any) -> "DataNode":
+        other = self.promote(other)
+        matmul_node = Matmul()
+        return matmul_node(other, self)
+    
     def __pow__(self, other: "DataNode" | Any) -> "DataNode":
         other = self.promote(other)
         pow_node = Pow()
         return pow_node(self, other)
     
-    def __truediv__(self, other: "DataNode" | Any) -> "DataNode":
+    def __rpow__(self, other: "DataNode" | Any) -> "DataNode":
         other = self.promote(other)
+        pow_node = Pow()
+        return pow_node(other, self)
+    
+    def __truediv__(self, other: "DataNode" | Any) -> "DataNode":
         return self * other ** -1
+    
+    def __rtruediv__(self, other: "DataNode" | Any) -> "DataNode":
+        return self ** -1 * other
     
     def __gt__(self, other: "DataNode" | Any) -> "DataNode":
         compare_node = Compare(operator.gt)
@@ -133,7 +149,27 @@ class DataNode(Node):
         return slice_node(self, slices)
     
     def __setitem__(self, slices: Union[slice, List[slice]]) -> "DataNode":
-        raise NotImplementedError("assignment currently not supported")
+        # TODO
+        raise NotImplementedError("assignment currently not supported") 
+    
+    def __neg__(self) -> "DataNode":
+        return self * -1
+    
+    def __pos__(self) -> "DataNode":
+        return self
+    
+    def __invert__(self) -> "DataNode":
+        invert_node = Invert()
+        return invert_node(self)
+    
+    def __str__(self) -> str:
+        return f"""DataNode(\ndata=\n{self.data},\ngrad=\n{self.grad}\n)"""
+    
+    def __repr__(self) -> str:
+        return str(self)
+    
+    def __hash__(self) -> str:
+        return id(self)
     
     # Useful ops from numpy
     
@@ -141,14 +177,14 @@ class DataNode(Node):
         reshape_node = Reshape()
         return reshape_node(self, new_shape)
     
-    # str and repr
-
-    def __str__(self) -> str:
-        return f"""DataNode(\ndata=\n{self.data},\ngrad=\n{self.grad}\n)"""
+    def max(self, other: "DataNode" | Any) -> "DataNode":
+        mask = self >= other
+        return mask * self + ~mask * other
     
-    def __repr__(self) -> str:
-        return str(self)
-    
+    def min(self, other: "DataNode" | Any) -> "DataNode":
+        mask = self <= other
+        return mask * self + ~mask * other
+        
 
 ##########################
 # FunctionNode
@@ -166,24 +202,27 @@ class FunctionNode(Node):
     
     def forward(self, *inputs: DataNode | Any) -> DataNode:
         # TODO: handle broadcasting in here if needed -> HOW DO U KNOW? IDK BRO 😭 
-        # -> WAIT I'M GOATED: DO YOU EVEN NEED TO TRACK SHAPES IF THE ACCUMULATE GRAD DOES IT'S JOB ??? IDK BRO WE TRY
+        # -> WAIT I'M GOATED: DO YOU EVEN NEED TO TRACK SHAPES IF 
+        # THE ACCUMULATE GRAD DOES IT'S JOB ??? IDK BRO WE TRY
 
         # attach inputs
         input_data_nodes = [i for i in inputs if isinstance(i, DataNode)]
         self.add_children(*input_data_nodes)
 
-        # create new DataNode and add itself as its source
-        forward_inputs = [i.data if isinstance(i, DataNode) else i for i in inputs]
+        # get data for new DataNode
+        forward_inputs = [
+            i.data if isinstance(i, DataNode) else i
+            for i in inputs]
         data = self._forward(*forward_inputs)
-        if not self._is_differentiable:
-            # result detached from computation graph
+
+        # result detached from computation graph
+        if not self._is_differentiable or\
+        not any(i.requires_grad for i in input_data_nodes):
             new_data_node = DataNode(data, requires_grad=False)
             return new_data_node
         
-        new_data_node = DataNode(
-            data,
-            requires_grad=any(i.requires_grad for i in input_data_nodes)
-            )
+        # add self (function node) as child of the new data node
+        new_data_node = DataNode(data, requires_grad=True)
         new_data_node.add_children(self)
 
         # attach created DataNode as result
@@ -206,7 +245,7 @@ class FunctionNode(Node):
         raise NotImplementedError
 
     @property
-    def _is_differentiable(self):
+    def _is_differentiable(self) -> bool:
         # set to false to disable gradient tracking
         # must not use backward_if thats the case
         return True
@@ -285,7 +324,16 @@ class Compare(FunctionNode):
         return self.op(a, b)
     
     @property
-    def _is_differentiable(self):
+    def _is_differentiable(self) -> bool:
+        return False
+    
+class Invert(FunctionNode):
+
+    def _forward(self, a: ndarray) -> ndarray:
+        return ~a
+    
+    @property
+    def _is_differentiable(self) -> bool:
         return False
     
 # Useful numpy functions
@@ -300,13 +348,10 @@ class Reshape(FunctionNode):
         (old_shape, ) = self.backprop_assets
         return (partial.reshape(old_shape), )
     
-# Other
-
-class BoolMask(FunctionNode):
+class Reduce(FunctionNode):
     # TODO
     def _forward(self, *inputs: ndarray | Any) -> ndarray:
         return super()._forward(*inputs)
     
     def _backward(self, *partials: ndarray) -> Tuple[ndarray]:
         return super()._backward(*partials)
-    
